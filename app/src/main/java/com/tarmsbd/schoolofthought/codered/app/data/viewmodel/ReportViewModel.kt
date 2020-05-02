068e248b8f261c9d18e1dc730196e3ebc1abc1f1
Rs Robin <srrobin33@gmail.com>
2020-05-02 20:28:54 +0600

    report page design

--- app/src/main/java/com/tarmsbd/schoolofthought/codered/app/data/viewmodel/ReportViewModel.kt
+++ app/src/main/java/com/tarmsbd/schoolofthought/codered/app/data/viewmodel/ReportViewModel.kt
@@ -1,12 +1,22 @@
 package com.tarmsbd.schoolofthought.codered.app.data.viewmodel
 
+import android.app.DatePickerDialog
+import android.util.Log
+import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.tarmsbd.schoolofthought.codered.app.data.models.Report
 import com.tarmsbd.schoolofthought.codered.app.data.repository.FirebaseRepo
+import java.util.*
 
 class ReportViewModel : ViewModel() {
+
+    private val c = Calendar.getInstance()
+    private val year = c.get(Calendar.YEAR)
+    private val month = c.get(Calendar.MONTH)
+    private val day = c.get(Calendar.DAY_OF_MONTH)
+
 
     private var mReportData = MutableLiveData<Report>()
     private var report: Report = Report()
@@ -46,5 +56,18 @@ class ReportViewModel : ViewModel() {
         this.report = Report()
 
         mReportData.value = report
+    }
+    fun showDatePicker(view: View) {
+        DatePickerDialog(
+            view.context,
+            DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+                // do stuff
+                Log.d(TAG, "DatePickerDialog: $year/${monthOfYear + 1}/$dayOfMonth")
+
+            },
+            year,
+            month,
+            day
+        ).show()
     }
 }
